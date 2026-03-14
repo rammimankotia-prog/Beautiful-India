ef3869c3899e2815b04df182097ce28c94abacd5
Raman <rammimankotia@gmail.com>
2026-03-14 16:19:29 +0530

    Final Deploy: Secrets and public_html verified

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Automated deployment triggered: 2026-03-14 16:11:00
+// Automated deployment triggered: 2026-03-14 16:21:00
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
