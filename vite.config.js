e0114cbff9786b7775f7a7c515b0d51aff6517ab
Raman <rammimankotia@gmail.com>
2026-03-14 17:00:13 +0530

    Final Deploy: Separate secrets confirmed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Automated deployment triggered: 2026-03-14 16:35:00
+// Automated deployment triggered: 2026-03-14 17:01:00
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
